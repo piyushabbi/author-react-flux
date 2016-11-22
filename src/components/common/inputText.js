02f5e1a8de0324e35b12c17ae7025a0356d4bc00
Piyush Abbi <pabbi@sapient.com>
2016-11-22 19:33:41 +0530

    added error handling condition for markup update and proptypes

--- src/components/common/inputText.js
+++ src/components/common/inputText.js
@@ -3,9 +3,26 @@
 var React = require('react');
 
 var Input = React.createClass({
+
+    propTypes: {
+        name: React.propTypes.string.isRequired,
+        label: React.propTypes.string.isRequired,
+        onChange: React.propTypes.func.isRequired,
+        placeholder: React.propTypes.string,
+        value: React.propTypes.string,
+        error: React.propTypes.string
+    },
+
     render: function() {
+        //Add extra class at parent node in case of error
+        var wrapperClass = 'form-group';
+
+        if (this.props.error && this.props.error.length > 0) {
+            wrapperClass += ' ' + 'has-error';  //String Concatination for error
+        }
+
         return (
-            <div>
+            <div className={ wrapperClass }>
                 <label htmlFor={ this.props.name }>{ this.props.label }</label>
                 <input type="text"
                     className="form-control"
@@ -13,6 +30,8 @@ var Input = React.createClass({
                     placeholder={ this.props.placeholder }
                     value={ this.props.value }
                     onChange={ this.props.onChange } />
+
+                <div className="input"> { this.props.error } </div>
             </div>
         );
     }
